552951dfefca2448de175f6e1c933913de5d1c3e
Henrique Picanço <114828539+henriquepicanco@users.noreply.github.com>
2023-12-03 11:52:42 -0300

    feat(fish): add fisher and some plugins to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_config theme save "Catppuccin Mocha"
+	# Commands to run in interactive sessions can go here
+	fish_config theme choose "Dracula Official"
 end
