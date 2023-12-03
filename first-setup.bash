85df81e5651d39b5815c6442dd0f9422bc1dad30
Henrique Picanço <114828539+henriquepicanco@users.noreply.github.com>
2023-12-03 11:54:16 -0300

    feat(first-setup): add script to automatize dotfiles

---
+++ first-setup.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script should be used when a new environment is configured.
+
+# Fish
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
+
+# Git
+ln -s ~/.dotfiles/.config/git ~/.config/git
+
+# tmux
+ln -s ~/.dotfiles/.tmux ~/.tmux
+ln -s ~/.dotfiles/.config/tmux ~/.config/tmux
+
+# nvim
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
