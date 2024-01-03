4466092caa641d5be2df0a0bf176a17e2ec618d4
Henrique Picanço <114828539+henriquepicanco@users.noreply.github.com>
2024-01-03 14:56:59 -0300

    update(first-setup): update first-setup.sh script

--- first-setup.bash
+++ first-setup.bash
@@ -3,14 +3,24 @@
 # This script should be used when a new environment is configured.
 
 # Fish
+## Symlink the folder from .dotfiles to .config 
 ln -s ~/.dotfiles/.config/fish ~/.config/fish
 
+## Install the Fisher plugin manager
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+## Install plugins via fisher
+fisher install catppuccin/fish
+
 # Git
+## Symlink the folder from .dotfiles to .config
 ln -s ~/.dotfiles/.config/git ~/.config/git
 
 # tmux
+## Symlink the folders from .dotfiles to .config and .tmux
 ln -s ~/.dotfiles/.tmux ~/.tmux
 ln -s ~/.dotfiles/.config/tmux ~/.config/tmux
 
-# nvim
+# Neovim
+## Symlink the folders from .dotfiles to .config
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
